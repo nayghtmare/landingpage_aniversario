412c877c2516566b958a744cf4a9653a83ab8292
Nayara Stelmach <nayaradesigner@gmail.com>
2025-10-31 19:36:54 -0300

    final

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+const dataDoEvento = new Date("Jan 11, 2026 23:59:59");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor (distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);          
+    
+
+
+document.getElementById('contador').innerHTML = `${diasAteOEvento} dias, ${horasAteOEvento} horas, ${minutosAteOEvento} minutos e ${segundosAteOEvento} segundos`;
+
+
+if (distanciaAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById("contador").innerHTML = "O evento já aconteceu!";
+}
+
+}, 1000);
+
+
